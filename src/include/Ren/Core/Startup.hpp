18e0de6ad9dc7e4869c448f5ec00e8d67615eb9d
TheRetikGM <theretikgm@gmail.com>
2022-09-22 16:14:25 +0200

    Uncommented exception handling

--- src/include/Ren/Core/Startup.hpp
+++ src/include/Ren/Core/Startup.hpp
@@ -7,22 +7,22 @@ int main(int argc, char* argv[])
 {
     Ren::GameCore* game = CreateGame();
 
-    // try
-	// {
+    try
+	{
 		game->Init();
 		game->Loop();
 		game->Destroy();
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << "Exception caught! Message: " << e.what() << '\n';
-	// 	return 1;
-	// }
-	// catch(...)
-	// {
-	// 	std::cerr << "Invalid exception caught!" << '\n';
-	// 	return 2;
-	// }
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception caught! Message: " << e.what() << '\n';
+		return 1;
+	}
+	catch(...)
+	{
+		std::cerr << "Invalid exception caught!" << '\n';
+		return 2;
+	}
 
 	delete game;
 	return 0;
